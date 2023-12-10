35e3c7d29f58fdd87c718202c29f2b82aaebbbb7
amirafathy-tech <amira.fathy@solex.tech>
2023-12-10 17:16:11 +0200

    Displaying List of Recipes and details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[]=[
+    new Recipe('A Test Recipe','simply recipe','https://chatelaine.com/wp-content/uploads/2017/08/Grilled-steak-recipe-with-caesar-salad.jpg'),
+    new Recipe('A Test Recipe', ' simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ]
 }
